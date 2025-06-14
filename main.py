4a1d3a0c92bbdfee3fd06e394344e1ea3e3e227f
Kyriakos Kypraios <109945212+kyrkyp@users.noreply.github.com>
2025-06-14 18:02:41 +0300

    Add pdf generation

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import glob
+from fpdf import FPDF
+from pathlib import Path
+
+
+filepaths = glob.glob("invoices/*.xlsx")
+
+
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    filename = Path(filepath).stem
+    invoice_nr = filename.split("-")[0]
+    pdf.set_font("Times", size=16)
+    pdf.cell(50, 8, text=f"Invoice nr.{invoice_nr}")
+    pdf.output(f"invoices/{filename}.pdf")
